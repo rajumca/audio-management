9f87f564ec91531d5a15a91895a95438cf542ab0
rajumca <rajugriet483@gmail.com>
2018-04-28 18:11:49 -0500

    used Album class and Song class--normalized

--- src/app/service/album.service.ts
+++ src/app/service/album.service.ts
@@ -1,3 +1,4 @@
+import { Album } from '../album/album';
 import { Injectable } from '@angular/core';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
@@ -8,7 +9,7 @@ export class AlbumService {
 
  constructor( private http: HttpClient) { }
 
-  getAlbums():  Observable<String> {
-   return this.http.get<String>('http://localhost:8080');
+  getAlbums():  Observable<Album> {
+   return this.http.get<Album>('http://localhost:8080');
 }
 }
